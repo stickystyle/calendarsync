1907049f42ecd4fb23bd350865ff567af33c867c
Ryan Parrish <rparrish@nxt.tech>
2024-11-20 20:24:39 -0500

    Rework to use not use the SRC calendar from my iCloud account, since the end goal is to remove that.

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-import datetime
 import logging
-from datetime import datetime
+from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
-
+from icalevents.icalevents import events
 import caldav
 import environs
 
@@ -18,54 +17,19 @@ logging.basicConfig(level=env("LOG_LEVEL", "INFO"))  # Set the desired logging l
 #  This would be resolved the next time the script runs though.
 
 
-def sync_calendar(src_cal, dest_cal):
-    # Fetch all events from both calendars
-    src_events = [
-        event
-        for event in src_cal.events()
-        if event.vobject_instance.vevent.dtstart.value >= datetime.now(tz=my_tz)
-    ]
-    dest_events = [
-        event
-        for event in dest_cal.events()
-        if event.vobject_instance.vevent.dtstart.value >= datetime.now(tz=my_tz)
-    ]
+def sync_calendar(src_events_dict, dest_events_dict, destination_cal):
 
-    # Create dictionaries for quick lookup
-    src_event_dict = {
-        event.vobject_instance.vevent.uid.value: event for event in src_events
-    }
-    dest_event_dict = {
-        event.vobject_instance.vevent.uid.value: event for event in dest_events
-    }
-
-    # 1. Remove any events that are not in the SRC calendar
-    for uid, dest_event in dest_event_dict.items():
-        if uid not in src_event_dict:
+    # Remove any events that are not in the SRC calendar
+    for event_id, dest_event in dest_events_dict.items():
+        if event_id not in src_event_dict:
             logging.info("Deleting event: %s", dest_event)
             dest_event.delete()
 
-    # 2. Check for updates to existing events
-    for uid, src_event in src_event_dict.items():
-        if uid in dest_event_dict:
-            dest_event = dest_event_dict[uid]
-            src_vevent = src_event.vobject_instance.vevent
-            dest_vevent = dest_event.vobject_instance.vevent
-
-            # Check if the event has changed
-            if (
-                src_vevent.contents["last-modified"]
-                != dest_vevent.contents["last-modified"]
-            ) and (src_vevent.contents["summary"] != dest_vevent.contents["summary"]):
-                logging.info("Updating event: %s", src_event)
-                dest_event.delete()
-                dest_cal.add_event(src_event.data)
-
-    # 3. Add any new events from the SRC calendar to the DEST calendar
-    for uid, src_event in src_event_dict.items():
-        if uid not in dest_event_dict:
+    # Add any new events from the SRC calendar to the DEST calendar
+    for event_id, src_event in src_events_dict.items():
+        if event_id not in dest_event_dict:
             logging.info("Adding event: %s", src_event)
-            dest_cal.add_event(src_event.data)
+            destination_cal.add_event(src_event.component.to_ical())
 
 
 with caldav.DAVClient(
@@ -73,11 +37,30 @@ with caldav.DAVClient(
     username=env("USER_NAME"),
     password=env("PASSWD"),
 ) as client:
-    my_principal = client.principal()
+    now = datetime.now(my_tz)
+    src_events = events(
+        env("SRC_CAL"), fix_apple=True, sort=True, end=now + timedelta(days=21)
+    )
+    src_event_dict = {
+        f"{event.summary}-{event.start.isoformat()}-{event.end.isoformat()}": event
+        for event in src_events
+    }
 
+    my_principal = client.principal()
     calendars = my_principal.calendars()
+
     logging.info("Filtering calendars")
-    work_cal = [c for c in calendars if c.name == env("SRC_CAL")][0]
-    family_cal = [c for c in calendars if c.name == env("DEST_CAL")][0]
+    dest_cal = [c for c in calendars if c.name == env("DEST_CAL")][0]
+    dest_events = [
+        event
+        for event in dest_cal.events()
+        if event.vobject_instance.vevent.dtstart.value >= datetime.now(tz=my_tz)
+    ]
+
+    dest_event_dict = {
+        f"{event.vobject_instance.vevent.summary.value}-{event.vobject_instance.vevent.dtstart.value.isoformat()}-{event.vobject_instance.vevent.dtend.value.isoformat()}": event
+        for event in dest_events
+    }
+
     logging.info("Syncing calendars")
-    sync_calendar(work_cal, family_cal)
+    sync_calendar(src_event_dict, dest_event_dict, dest_cal)
